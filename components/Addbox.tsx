0b6e72e68f4ff0c3bf3d0843ccdc7476976fa9b3
Jenisha Khulal <jenishakhulal@gmail.com>
2025-03-03 18:12:20 +0545

    Addbox component modified to include add box for card and deck

--- components/Addbox.tsx
+++ components/Addbox.tsx
@@ -2,61 +2,80 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 import decks from '@/data/decks'
 import cards from '@/data/cards'
+import { AddboxProps } from '@/types/types'
 
-type AddboxProps = {
-  deckID: number;
-  setAdd: React.Dispatch<React.SetStateAction<boolean>>;
-  add: boolean;
-};
 
 const Addbox = ({
-  deckID,setAdd,add
+  deckID,setAdd,add,card
 }: AddboxProps) => {
 
-  const [form, setForm] = useState({
+  const [CardForm, setCardForm] = useState({
     word: "",
     meaning: "",
+  });
+
+  const [DeckForm, setDeckForm] = useState({
+    deckName: ""
   });
   
-  const [error, setError] = useState({
+  const [cardError, setCardError] = useState({
     word: "",
     meaning: "",
   });
 
-  const {word,meaning}=form
+  const [deckError, setDeckError] = useState({
+    deckName: ""
+  });
+
+  const { word, meaning } = CardForm
+  const {deckName} = DeckForm
  
 
-  const handleChange = (value:string, name: string) => {
-    setForm({ ...form, [name]: value });
+  const handleChange = (value:string, name: string,card:boolean) => {
+    card && setCardForm({ ...CardForm, [name]: value });
+    !card && setDeckForm({...DeckForm,[name]:value})
   };
 
 
 
-  const handleSubmit = ()=>{
-    if (form.word == "" || form.meaning=="") {
-      let tempError=error
-      if (!form.word) tempError = {...error,["word"]:"Add the word"}
-      if (!form.meaning) setError({...tempError, ["meaning"]:"Add the meaning"})
+  const handleSubmitCard = ()=>{
+    if (CardForm.word == "" || CardForm.meaning=="") {
+      let tempError=cardError
+      if (!CardForm.word) tempError = {...cardError,["word"]:"Word is required"}
+      if (!CardForm.meaning) setCardError({...tempError, ["meaning"]:"Meaning is required"})
     }
     else {
       console.log("word:",word,"meaning:",meaning,deckID)
       decks[deckID].cards.push({
-        id: 10,
+        id: decks[deckID].cards.length,
         word: word,
         meaning: meaning,
       });
       cards.push({
-        id: 10,
-        word: form.word,
-        meaning: form.meaning,
+        id: decks[deckID].cards.length,
+        word: word,
+        meaning:meaning,
       });
       setAdd(!add);
     }
-    
-    
-
-
   }
+
+  const handleSubmitDeck = () => {
+    if (DeckForm.deckName == "" ) {
+  
+        setDeckError({ ...deckError, ["deckName"]: "Deck Title is required" });
+    } else {
+      console.log("Deck Name",deckName ,deckID);
+      decks.push({
+        id:decks.length,
+        title: deckName,
+        cards:[]
+      });
+      setAdd(!add);
+    }
+  };
+
+  
   return (
     <View className="absolute top-48 left-20 border-2  w-64 z-10 flex rounded-xl bg-gray-50 shadow-slate-500">
       <View className="flex items-end ">
@@ -64,33 +83,64 @@ const Addbox = ({
           <Button title="X" color="gray" onPress={() => setAdd(!add)} />
         </View>
       </View>
-      <View className=" gap-2 items-center justify-center p-4">
-        <View className="w-full">
-          <TextInput
-            className="w-full h-12 rounded-md border bg-white"
-            onChangeText={(value) => handleChange(value, "word")} 
-            value={form.word}
-            placeholder="word"
-          />
-          {error.word && (
-            <Text className="text-sm text-red-400">{error.word}</Text>
-          )}
-        </View>
+      {card && (
+        <View className=" gap-2 items-center justify-center p-4">
+          <View className="w-full">
+            <TextInput
+              className="w-full h-12 rounded-md border bg-white"
+              onChangeText={(value) => handleChange(value, "word", true)}
+              value={CardForm.word}
+              placeholder="word"
+            />
+            {cardError.word && (
+              <Text className="text-sm text-red-400">{cardError.word}</Text>
+            )}
+          </View>
 
-        <View className="w-full">
-          <TextInput
-            className="w-full h-12 rounded-md border bg-white"
-            onChangeText={(value) => handleChange(value, "meaning")}
-            value={form.meaning}
-            placeholder="meaning"
-          />
-          {error.meaning && (
-            <Text className="text-sm text-red-400">{error.meaning}</Text>
-          )}
-        </View>
+          <View className="w-full">
+            <TextInput
+              className="w-full h-12 rounded-md border bg-white"
+              onChangeText={(value) => handleChange(value, "meaning", true)}
+              value={CardForm.meaning}
+              placeholder="meaning"
+            />
+            {cardError.meaning && (
+              <Text className="text-sm text-red-400">{cardError.meaning}</Text>
+            )}
+          </View>
 
-        <Button title="Add" color="green" onPress={() => handleSubmit()} />
-      </View>
+          <Button
+            title="Add"
+            color="green"
+            onPress={() => handleSubmitCard()}
+          />
+        </View>
+      )}
+
+      {!card && (
+        <View className=" gap-2 items-center justify-center p-4">
+          <View className="w-full">
+            <TextInput
+              className="w-full h-12 rounded-md border bg-white"
+              onChangeText={(value) => handleChange(value, "deckName", false)}
+              value={DeckForm.deckName}
+              placeholder="Title of the Deck"
+            />
+            {deckError.deckName && (
+              <Text className="text-sm text-red-400">{deckError.deckName}</Text>
+            )}
+          </View>
+
+          <Button
+            title="Add"
+            color="green"
+            onPress={() => {
+              card && handleSubmitCard()
+              !card && handleSubmitDeck()
+            }}
+          />
+        </View>
+      )}
     </View>
   );
 };
