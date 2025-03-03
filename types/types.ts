7600d8a7dff58ed542d0f8374e0bfb66063619f8
Jenisha Khulal <jenishakhulal@gmail.com>
2025-03-03 18:11:31 +0545

    types file added: consists are defined types

---
+++ types/types.ts
@@ -0,0 +1,20 @@
+type CardProps = {
+  word: string;
+  meaning: string;
+  id: number;
+};
+
+type DeckComponentProps = {
+  title: string;
+    id: number;
+  cards: CardProps[]
+};
+
+type AddboxProps = {
+  deckID: number;
+  setAdd: React.Dispatch<React.SetStateAction<boolean>>;
+  add: boolean;
+  card:boolean
+};
+
+export {CardProps, DeckComponentProps, AddboxProps}
